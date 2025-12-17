ec49d8bc2e73cbb43e224459e33090be1e0b54fb
Tuncer Schmidt <53016813+TuncerSchmidt@users.noreply.github.com>
2025-12-16 23:32:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def try_analysis(symbol, exchange):
     handler = TA_Handler(
         symbol=symbol.upper(),
         screener="america",
-        exchange=exchange,
+        exchange="NASDAQ",
         interval=Interval.INTERVAL_1_DAY
     )
     return handler.get_analysis()
@@ -25,17 +25,17 @@ def try_analysis(symbol, exchange):
 def get_tv_analysis(symbol: str):
 
     # 1️⃣ Tüm borsaları sırayla dene
-    for ex in EXCHANGES:
-        try:
-            analysis = try_analysis(symbol, ex)
-            return {
-                "symbol": symbol.upper(),
-                "exchange": ex,
-                "summary": analysis.summary,
-                "indicators": analysis.indicators
-            }
-        except Exception:
-            continue  # sıradaki exchange’e geç
+    # for ex in EXCHANGES:
+    try:
+        analysis = try_analysis(symbol, ex)
+        return {
+            "symbol": symbol.upper(),
+            "exchange": ex,
+            "summary": analysis.summary,
+            "indicators": analysis.indicators
+        }
+    except Exception:
+        continue  # sıradaki exchange’e geç
 
     # 2️⃣ Eğer hepsi hata verirse → fallback: exchange=None
     try:
